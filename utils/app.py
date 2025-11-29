345519ec46a917ae59233e68cff8a873b648bc1a
ViniMPC <vini007.m.m@gmail.com>
2025-11-29 08:20:57 -0300

    Update

--- utils/app.py
+++ utils/app.py
@@ -1,4 +1,4 @@
-from tkinter import Tk
+from tkinter import Tk, Label, Button
 
 class App:
     def __init__(self):
@@ -6,9 +6,13 @@ class App:
         self.root.title("My Application")
         self.root.geometry("400x300")
 
-    
+
+    def setup_gui(self):
+        title_label = Label(self.root, text="Welcome to My Application", font=("Helvetica", 16))
+        title_label.pack(pady=20)
 
     def run(self):
+        self.setup_gui()
         self.root.mainloop()
 
 
